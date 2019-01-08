1ea26de393423bebdc35d1fc8f5fe878fdc3c32d
bbchip0103 <bbchip13@gmail.com>
2019-01-08 16:22:28 +0900

    Add test code 'calculate.py'

--- calculate.py
+++ calculate.py
@@ -1,5 +1,14 @@
+def devide(a, b):
+    pass
+
+def multiply(a, b):
+    pass
+
 def add(a, b):
     return a+b
 
 if __name__=='__main__':
     print('???:', add(1,2))
+    print('multiply:', multiply(3,4))
+    print('devide:', devide(3,4))
+
