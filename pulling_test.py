19f9f359a90138d574e5063fd2f3ce824603603d
bbchip0103 <bbchip13@gmail.com>
2019-01-08 17:33:51 +0900

    Update files

--- pulling_test.py
+++ pulling_test.py
@@ -1,4 +1,15 @@
-import calculate
+from user_inform import collect_inform
 
+def is_collect_inform(_id, password):
+    return True if (_id==collect_inform['_id'] \
+                    and password==collect_inform['passwd']) \
+                        else False
+    
 if __name__=='__main__':
-    print('result:', calculate.add(3,4))
+    _id = input('id:')
+    passwd = input('passwd:')
+
+    if is_collect_inform(_id, passwd) == True:
+        print('It is collect inform')
+    else:
+        print('It is incollect inform')
